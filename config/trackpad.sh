74b9d5bf67d6cc2a57ab9479196f026fdbdfd405
Cris Stanza <crisstanza@gmail.com>
2014-03-01 15:18:33 -0300

    Update trackpad.sh

--- config/trackpad.sh
+++ config/trackpad.sh
@@ -1,3 +1,4 @@
 synclient VertScrollDelta=-110
 synclient HorizScrollDelta=-110
 synclient HorizTwoFingerScroll=1
+gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled "true"
