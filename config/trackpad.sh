7f46c5b65ef61dee02d6acb10a7013b77f628ffd
Cris Stanza <crisstanza@gmail.com>
2014-03-01 16:46:36 -0300

    Update trackpad.sh

--- config/trackpad.sh
+++ config/trackpad.sh
@@ -1,5 +1,5 @@
 # xinput list
 # xinput list-props <DEVICE NUMBER>
-synclient AccelFactor=0.04 HorizScrollDelta=-110 HorizTwoFingerScroll=1 MaxSpeed=2 MinSpeed=1.5 VertScrollDelta=-110
+synclient AccelFactor=0.05 HorizScrollDelta=-110 HorizTwoFingerScroll=1 MaxSpeed=5 MinSpeed=2.5 VertScrollDelta=-110
 # synclient -l | sort | grep "AccelFactor\|HorizScrollDelta\|HorizTwoFingerScroll\|MaxSpeed\|MinSpeed\|VertScrollDelta"
 gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled "true"
